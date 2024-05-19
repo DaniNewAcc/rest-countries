7bb9e37e2ea83ba6adb6de34d019ed9242d1a58f
Daniele <dddaaaniii.98@gmail.com>
2024-05-19 03:18:33 +0200

    fix: flickering screen on dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import Details from './pages/Details';
-import { useEffect } from 'react';
-import { useThemeContext } from './hooks/useThemeContext';
 
 
 
 function App() {
-  const { theme } = useThemeContext();
-    useEffect(() => {
-    if (theme === 'dark') {
-      document.body.style.backgroundColor = 'hsl(207, 26%, 17%)'
-    } else {
-      document.body.style.backgroundColor = 'white'
-    }
-  }, [theme])
   
   return (
       <HashRouter>
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -6,20 +6,21 @@ export const ThemeContext = createContext();
 
 export function ThemeContextProvider ({ children }) {
     const initialTheme = () => localStorage.getItem('theme');
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
     const [theme, setTheme] = useState(initialTheme);
     const toggleTheme = () => {
         setTheme((theme) => (theme === 'light' ? 'dark' : 'light'));
     }
     useLayoutEffect(() => {
         localStorage.setItem('theme', theme);
-        if (theme === 'light') {
-            document.documentElement.classList.remove('dark');
-            document.documentElement.classList.add('light');
-        } else {
+        if (theme === 'dark' || (!theme && systemTheme)) {
             document.documentElement.classList.remove('light');
             document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            document.documentElement.classList.add('light');
         }
-    }, [theme])
+    }, [theme, systemTheme])
 
     
     return (
